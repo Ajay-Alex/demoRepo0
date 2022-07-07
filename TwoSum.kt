1560e3062dacd155cd75450f78154bc7d5a32375
Ajay-Alex <ajay.alex@theporter.in>
2022-07-07 13:32:50 +0530

    added twoSum fn body

--- TwoSum.kt
+++ TwoSum.kt
@@ -6,8 +6,24 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 */
 fun main(){
+    println("**START**")
     var nums= arrayOf(3,2,4)
+    twoSum(nums,6)
 }
 fun twoSum(nums: Array<Int>,target: Int){
+    var ans= IntArray(2)
+    print(nums)
+    for(i in 0 until nums.size-2){
+        for(j in i+1 until nums.size-1){
+            if(i+j==target) {
+                println("[$i,$j]")
+                ans[0]=i
+                ans[1]=j
+                print(ans)
+                break
+            }
 
+        }
+    }
+    print("No ans")
 }
