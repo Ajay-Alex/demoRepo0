033b78b0a7f1c0eceed44a039e12aed5e3f2385b
Ajay-Alex <ajay.alex@theporter.in>
2022-07-08 13:31:38 +0530

    lambda

---
+++ Lambda.kt
@@ -0,0 +1,22 @@
+fun main(){
+    lambda()
+
+
+}
+fun lambda(){
+    var dirtyLevel = 20
+    val waterFilter = { dirty : Int -> dirty / 2}
+    println(waterFilter(dirtyLevel))
+
+    val waterFilter2:(Int)->Float={dirty2->(dirty2.toFloat()/3)}
+    println(waterFilter2(dirtyLevel))
+    println(updateDirty(dirtyLevel,waterFilter2))
+    println(updateDirty(dirtyLevel,::increaseDirty))
+
+}
+
+fun updateDirty(dirty: Int, operation: (Int) -> Float): Float {
+    return operation(dirty/3)
+}
+
+fun increaseDirty(start:Int):Float=start.toFloat()*5.5f
