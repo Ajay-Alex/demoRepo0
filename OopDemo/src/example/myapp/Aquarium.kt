4c0970daa5c2e695f6b33917551250d39b75f340
Ajay-Alex <ajay.alex@theporter.in>
2022-07-11 11:52:25 +0530

    Inheritance

--- OopDemo/src/example/myapp/Aquarium.kt
+++ OopDemo/src/example/myapp/Aquarium.kt
@@ -1,20 +1,24 @@
 package OopDemo.src.example.myapp
 
-class Aquarium(var length: Int = 0, var width: Int = 0, var height: Int = 0) {
+open class Aquarium(var length: Int = 0, var width: Int = 0, open var height: Int = 0) {
     init {
         println("aquarium initializing")
         //println("Volume: ${width * length * height / 1000} l")
     }
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             if(length!=0&&width!=0)height = (value * 1000) / (width * length)
         }
+    open val shape ="rectangle"
+    open var water: Double = 0.0
+        get() = volume * 0.9
     fun printSize() {
+        println(shape)
         println("""Width: $width cm ,
             |Length: $length cm ,
             |Height: $height cm """.trimMargin())
-        println("Volume: $volume l")
+        println("Volume: $volume l Water: $water l (${water / volume * 100.0}% full)")
     }
 
 }
--- OopDemo/src/main/kotlin/main.kt
+++ OopDemo/src/main/kotlin/main.kt
@@ -1,14 +1,15 @@
 package OopDemo.src.main.kotlin
 
 import OopDemo.src.example.myapp.Aquarium
+import OopDemo.src.example.myapp.TowerTank
 
 fun main() {
     buildAquarium()
 }
 
 fun buildAquarium(){
-    val aquarium6= Aquarium(10,10,10)
-    aquarium6.printSize()
-    aquarium6.volume = 70
-    aquarium6.printSize()
+    val aquarium= Aquarium(25,25,40)
+    aquarium.printSize()
+    val tower = TowerTank(40,25)
+    tower.printSize()
 }
