bcf31032d0987bd109b65987a08f1ebe92c3f094
Ajay-Alex <ajay.alex@theporter.in>
2022-07-12 16:20:00 +0530

    tests

--- OopDemo/src/main/kotlin/main.kt
+++ OopDemo/src/main/kotlin/main.kt
@@ -4,19 +4,34 @@ import OopDemo.src.example.myapp.aquarium.Aquarium
 import OopDemo.src.example.myapp.aquarium.Plecostomus
 import OopDemo.src.example.myapp.aquarium.Shark
 import OopDemo.src.example.myapp.aquarium.TowerTank
-import OopDemo.src.example.myapp.decor.makeDecorations
 
 fun main() {
-    //buildAquarium()
     //makeFish()
-    makeDecorations()
+    //makeDecorations()
+    val list:MutableList<Any> = MutableList(5){idx -> 0}
+    list.add(2,"asasas")
+    var arr:ArrayList<Any> = ArrayList()
+    arr.add("fgfgfg")
+    arr.add(22)
+    arr.add(0,9.8)
+    //println(arr.size)
+    list.add(5)
+    val decorations = mutableListOf( 1,"pagoda", "plastic plant", "alligator", "flowerpot")
+    decorations.add(5)
+    buildAquarium(arr)
+
+    //println(decorations[5])
+
 }
 
-fun buildAquarium(){
-    val aquarium= Aquarium(25,25,40)
-    aquarium.printSize()
-    val tower = TowerTank(40,25)
-    tower.printSize()
+fun buildAquarium(array: MutableList<Any>){
+    for(i in array){
+        println(i)
+    }
+    //val aquarium= Aquarium(25,25,40)
+    //aquarium.printSize()
+    //val tower = TowerTank(40,25)
+    //tower.printSize()
 }
 fun makeFish() {
     val shark = Shark()
